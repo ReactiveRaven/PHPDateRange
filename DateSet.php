3022d6591c7b99df0eb4a462dab4d15f6c2d585f
David Godfrey <reactiveraven@gmail.com>
2013-07-01 17:35:31 +0100

    starting implementing sorting

--- DateSet.php
+++ DateSet.php
@@ -4,13 +4,13 @@ require_once 'DateRange.php';
 
 class DateSet
 {
-  const SORT_INSERTION = "insertion";
-  const SORT_START_ASC = "start";
-  const SORT_START_DESC = "trats";
-  const SORT_END_ASC = "end";
-  const SORT_END_DESC = "dne";
-  const SORT_DATA_ASC = "data";
-  const SORT_DATA_DESC = "atad";
+  const SORT_INSERTION = "SORT_INSERTION";
+  const SORT_START_ASC = "SORT_START_ASC";
+  const SORT_START_DESC = "SORT_START_DESC";
+  const SORT_END_ASC = "SORT_END_ASC";
+  const SORT_END_DESC = "SORT_END_DESC";
+  const SORT_DATA_ASC = "SORT_DATA_ASC";
+  const SORT_DATA_DESC = "SORT_DATA_DESC";
   
   private $dateRanges = array();
   
@@ -87,14 +87,92 @@ class DateSet
     return $result;
   }
   
-  public function getDateRangesData()
+  public function getDateRangesData($sort_order = null)
   {
+    $dateRanges = $this->dateRanges;
+    
+    if ($sort_order !== null && $sort_order !== self::SORT_INSERTION) {
+      $dateRanges = $this->sort($dateRanges, $sort_order);
+    }
+    
     $result = array();
-    foreach ($this->dateRanges as $dateRange /* @var $dateRange DateRange */)
+    foreach ($dateRanges as $dateRange /* @var $dateRange DateRange */)
     {
       $result[] = $dateRange->getData();
     }
     
     return $result;
+  }
+  
+  private function sort($data, $sort_method) 
+  {
+    if (method_exists($this, strtolower($sort_method))) {
+      return call_user_method(strtolower($sort_method), $this, $data);
+    } 
+    
+    throw new InvalidArgumentException("Unrecognised sorting constant '" . $sort_method . "'");
+  }
+  
+  private function sort_data_asc($data)
+  {
+    $output = array();
+    $sortkeys = array();
+    
+    foreach ($data as $dateRange /* @var $dateRange DateRange */)
+    {
+      $output[] = $dateRange;
+      $sortkeys[] = $dateRange->getData();
+    }
+    
+    array_multisort($sortkeys, $output); 
+    
+    return $output;
+  }
+ 
+  private function sort_data_desc($data)
+  {
+    return array_reverse($this->sort_data_asc($data));
+  }
+  
+  private function sort_start_asc($data)
+  {
+    $output = array();
+    $sortkeys = array();
+    
+    foreach ($data as $dateRange /* @var $dateRange DateRange */)
+    {
+      $output[] = $dateRange;
+      $sortkeys[] = $dateRange->getStart()->format("U");
+    }
+    
+    array_multisort($sortkeys, $output); 
+    
+    return $output;
+  }
+  
+  private function sort_start_desc($data)
+  {
+    return array_reverse($this->sort_start_asc($data));
+  }
+  
+  private function sort_end_asc($data)
+  {
+    $output = array();
+    $sortkeys = array();
+    
+    foreach ($data as $dateRange /* @var $dateRange DateRange */)
+    {
+      $output[] = $dateRange;
+      $sortkeys[] = $dateRange->getEnd()->format("U");
+    }
+    
+    array_multisort($sortkeys, $output); 
+    
+    return $output;
+  }
+  
+  private function sort_end_desc($data)
+  {
+    return array_reverse($this->sort_end_asc($data));
   }
 }
